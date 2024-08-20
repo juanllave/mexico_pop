e2432b37bd6804eedbab83d0d42a90d0dcec58db
JC <juancloz@gmail.com>
2024-08-19 18:43:35 -0600

    Added filters,cleaned script, left it ready for future use.

--- Rscripts/01_mexico_pop.R
+++ Rscripts/01_mexico_pop.R
@@ -1,13 +1,19 @@
 # Load libraries
 library(tidyverse)
+library(scales)
 library(readxl)
+library(patchwork)
 
 # Set working directory
 setwd('~/Documents/repos/R/mexico_pop')
 dir  <- getwd()
 
-# Load dataset with the population per muncipality, age groups, and sex
-pop <- readxl::read_excel(paste0(dir,'/datasets','/','1_Grupo_Quinq_00_RM.xlsx'))
+# # Initial setup to choose what year and what state is to be analyzed
+# state <- 'Baja California'
+# yearan <- 2024
+
+# Load dataset with the population per municipality, age groups, and sex
+data <- read_excel(paste0(dir,'/datasets/1_Grupo_Quinq_00_RM.xlsx'))
 
 # Create a vector to translate columns names into English
 columns <- c('key', 'key_ent', 'ent', 'mun', 'sex', 'year',
@@ -17,31 +23,26 @@ columns <- c('key', 'key_ent', 'ent', 'mun', 'sex', 'year',
              'pop_75_79', 'pop_80_84', 'pop_85_mm', 'pop_total')
 
 # Change the columns names
-colnames(pop) <- columns
+colnames(data) <- columns
 
-# Clean the data to leave only the total for the period 2016-2024
-pop_t <- pop %>% 
-  filter(year > 2015) %>%
-  filter(year < 2025) %>% 
-  select(-c(7:24)) %>% 
-  group_by(key, key_ent, ent, mun, year) %>% 
-  summarise(pop_total = sum(pop_total))
+# Pivot the dataset from wide to long, add percentage column
+pop <- data %>% 
+  pivot_longer(
+  cols = starts_with('pop_') & !ends_with('total'),
+  names_to = 'age_group',
+  values_to = 'population'
+) %>% 
+  group_by(year) %>% 
+  mutate(pct = round(population/pop_total, 5)) %>% 
+  ungroup() %>%
+  mutate(pct = if_else(sex == 'HOMBRES', pct, -pct)) %>% # Make female values negative to show them on the left side of the plot
+  select(-7) # Remove total_pop column to clean the table
 
 # Create a female-only dataset
 pop_f <- pop %>% 
-  filter(sex == 'MUJERES') %>% 
-  filter(year > 2015) %>%
-  filter(year < 2025) %>% 
-  select(-c(7:24)) %>% 
-  group_by(key, key_ent, ent, mun, year) %>% 
-  summarise(pop_total = sum(pop_total))
+  filter(sex == 'MUJERES')
 
 # Create a male-only dataset
 pop_m <- pop %>% 
-  filter(sex == 'HOMBRES') %>% 
-  filter(year > 2015) %>%
-  filter(year < 2025) %>% 
-  select(-c(7:24)) %>% 
-  group_by(key, key_ent, ent, mun, year) %>% 
-  summarise(pop_total = sum(pop_total))
+  filter(sex == 'HOMBRES')
 
