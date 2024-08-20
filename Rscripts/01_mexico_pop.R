bb3fbeabb7ee105fd3315b7202852244abd8cdcd
JC <juancloz@gmail.com>
2024-08-20 12:20:17 -0600

    Simplified the script, elimnitated unnecesary libraries.

--- Rscripts/01_mexico_pop.R
+++ Rscripts/01_mexico_pop.R
@@ -1,22 +1,16 @@
 # Load libraries
 library(tidyverse)
-library(scales)
 library(readxl)
-library(patchwork)
 
 # Set working directory
 setwd('~/Documents/repos/R/mexico_pop')
-dir  <- getwd()
-
-# # Initial setup to choose what year and what state is to be analyzed
-# state <- 'Baja California'
-# yearan <- 2024
+dir  <- getwd
 
 # Load dataset with the population per municipality, age groups, and sex
 data <- read_excel(paste0(dir,'/datasets/1_Grupo_Quinq_00_RM.xlsx'))
 
 # Create a vector to translate columns names into English
-columns <- c('key', 'key_ent', 'ent', 'mun', 'sex', 'year',
+columns <- c('key', 'key_state', 'state', 'mun', 'sex', 'year',
              'pop_00_04', 'pop_05_09', 'pop_10_14', 'pop_15_19', 'pop_20_24',
              'pop_25_29', 'pop_30_34', 'pop_35_39', 'pop_40_44', 'pop_45_49',
              'pop_50_54', 'pop_55_59', 'pop_60_64', 'pop_65_69', 'pop_70_74',
@@ -35,14 +29,11 @@ pop <- data %>%
   group_by(year) %>% 
   mutate(pct = round(population/pop_total, 5)) %>% 
   ungroup() %>%
-  mutate(pct = if_else(sex == 'HOMBRES', pct, -pct)) %>% # Make female values negative to show them on the left side of the plot
+  mutate(sex = case_when( #Change the values from SPANISH to English
+    sex == 'HOMBRES' ~ 'Men',
+    sex == 'MUJERES' ~ 'Women'
+  )) %>% 
+  mutate(pct = if_else(sex == 'Men', pct, -pct)) %>% # Make female values negative to show them on the left side of the plot
   select(-7) # Remove total_pop column to clean the table
 
-# Create a female-only dataset
-pop_f <- pop %>% 
-  filter(sex == 'MUJERES')
-
-# Create a male-only dataset
-pop_m <- pop %>% 
-  filter(sex == 'HOMBRES')
-
+ 
